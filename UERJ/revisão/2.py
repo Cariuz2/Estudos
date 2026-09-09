7e01e604c22451c117f3407fc33a1ecc6a0e6570
Gustavo Cariuz <105025153+Cariuz2@users.noreply.github.com>
2026-09-09 15:03:40 -0300

    Revise input loop to find the smallest of 10 values
    Updated loop to accept user input and find the smallest value among 10 inputs, handling ValueError exceptions.

--- UERJ/revisão/2.py
+++ UERJ/revisão/2.py
@@ -1,11 +1,19 @@
-valores = 0
+valores = 1
 menor = None
 numero = 0
-while valores < 10:
+while valores < 11:
 
     try:
+        numero = int(input(f'Insira o {valores}º valor: '))
+        valores += 1
+        
+    except ValueError:
+        print('Valor não identificado')
+        continue
 
-        
-        
-    except:
-        ...
+    if menor == None:
+        menor = numero
+    elif numero < menor:
+        menor = numero
+
+print(f'O menor valor dentre os 10 é {menor}')
